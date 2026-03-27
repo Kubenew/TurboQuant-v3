5ea31182ea771896858e15c807f62a4f83be1b02
Felix <90440279+Kubenew@users.noreply.github.com>
2026-03-27 05:16:25 +0100

    Update __init__.py

--- src/turboquant/__init__.py
+++ src/turboquant/__init__.py
@@ -1,3 +1,18 @@
+"""
+TurboQuant-v3: Efficient INT4 quantization for LLMs.
+"""
+
 __version__ = "0.3.0"
-from .quantize import quantize_model
-# ... other exports
+
+from .quantize import (
+    QuantConfig,
+    turboquant_v3_compress,
+    turboquant_v3_decompress,
+)
+
+__all__ = [
+    "__version__",
+    "QuantConfig",
+    "turboquant_v3_compress",
+    "turboquant_v3_decompress",
+]
